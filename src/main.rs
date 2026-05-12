694185a075c1b0276f689c587ad94d73fd95a656
Raymundo7532 <woloblocrafael@gmail.com>
2026-05-12 18:37:23 +0700

    Dependency: Chanfed: crosstown_bus to lapin; Added: futures-lite

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,70 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use lapin::{
+    Connection, ConnectionProperties, BasicProperties, ExchangeKind,
+};
 
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
     pub user_id: String,
-    pub user_name: String
+    pub user_name: String,
 }
 
-pub struct UserCreatedHandler;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = Connection::connect(
+        "amqp://guest:guest@localhost:5672",
+        ConnectionProperties::default(),
+    )
+    .await?;
 
-impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    let channel = conn.create_channel().await?;
     
-    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
-        println!("Message received on handler 1: {:?}", message);
-        Ok(())
+    // Declare exchange
+    channel
+        .exchange_declare(
+            "user_created",
+            ExchangeKind::Topic,
+            lapin::options::ExchangeDeclareOptions::default(),
+            lapin::types::FieldTable::default(),
+        )
+        .await?;
+
+    let messages = vec![
+        UserCreatedEventMessage {
+            user_id: "1".to_owned(),
+            user_name: "2406404642-Amir".to_owned(),
+        },
+        UserCreatedEventMessage {
+            user_id: "2".to_owned(),
+            user_name: "2406404642-Budi".to_owned(),
+        },
+        UserCreatedEventMessage {
+            user_id: "3".to_owned(),
+            user_name: "2406404642-Cica".to_owned(),
+        },
+        UserCreatedEventMessage {
+            user_id: "4".to_owned(),
+            user_name: "2406404642-Dira".to_owned(),
+        },
+        UserCreatedEventMessage {
+            user_id: "5".to_owned(),
+            user_name: "2406404642-Emir".to_owned(),
+        },
+    ];
+
+    for msg in messages {
+        let payload = borsh::to_vec(&msg)?;
+        channel
+            .basic_publish(
+                "user_created",
+                "user.created",
+                lapin::options::BasicPublishOptions::default(),
+                &payload,
+                BasicProperties::default(),
+            )
+            .await?;
+        println!("Published: {:?}", msg);
     }
 
-    fn get_handler_action(&self) -> String {
-        todo!()
-    }
-}
-
-fn main() {
-    let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
-    
-    _ = p.publish_event("user_created".to_owned(), 
-            UserCreatedEventMessage {user_id: "1".to_owned(), user_name: "2406404642-Amir".to_owned()}
-        );
-
-    _ = p.publish_event("user_created".to_owned(), 
-            UserCreatedEventMessage {user_id: "2".to_owned(), user_name: "2406404642-Budi".to_owned()}
-        );
-
-    _ = p.publish_event("user_created".to_owned(), 
-            UserCreatedEventMessage {user_id: "3".to_owned(), user_name: "2406404642-Cica".to_owned()}
-        );
-
-    _ = p.publish_event("user_created".to_owned(), 
-            UserCreatedEventMessage {user_id: "4".to_owned(), user_name: "2406404642-Dira".to_owned()}
-        );
-
-    _ = p.publish_event("user_created".to_owned(), 
-            UserCreatedEventMessage {user_id: "5".to_owned(), user_name: "2406404642-Emir".to_owned()}
-        );
+    Ok(())
 }
